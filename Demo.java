a9c408a91656a39be6e1fc6208408d2dbb701a12
Iraianbu-11 <iraianbu011@gmail.com>
2024-03-10 11:48:28 +0530

    Print Numbers from 10 to 1

--- Demo.java
+++ Demo.java
@@ -6,5 +6,12 @@ public class Demo {
         for (int i =1; i <= 10; i++) {
             System.out.println(i);
         }
+
+        // Print Numbers from 10 to 1
+
+        for (int i =10; i >= 1; i--) {
+            System.out.println(i);
+        }
+
     }
 }
